9bf14a5f981c6e56475085c4a5174f98571fbc4b
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2019-08-16 17:55:46 -0300

    Tab bar bottom

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createSwitchNavigator, createStackNavigator, createAppContainer } from "react-navigation"
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs"
 
 import Login from './src/screens/Login'
 import SearchCap from './src/screens/SearchCap'
@@ -9,13 +10,27 @@ const LoginStack = createStackNavigator({ Login })
 const SearchCapStack = createStackNavigator({ SearchCap })
 const FeedbackStack = createStackNavigator({ Feedback })
 
+const TabBottom = createMaterialBottomTabNavigator(
+	{
+		SearchCap: { screen: SearchCapStack },
+		Feedback: { screen: FeedbackStack },
+	}, {
+		initialRouteName: 'SearchCap',
+		activeColor: '#f0edf6',
+		inactiveColor: '#3e2465',
+		shifting: true,
+		barStyle: { backgroundColor: '#f68121' },
+	}
+)
+
 export default createAppContainer(
 	createSwitchNavigator(
 		{
-			Login: LoginStack
+			Login: LoginStack,
+			App: TabBottom
 		},
 		{
-			initialRouteName: 'Login'
+			initialRouteName: 'App'
 		}
 	)
 )
